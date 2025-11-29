ed8750c1cb8c780398028aa567180a4bcdb1c4b1
MadhurSikarwar <madhursikarwar1234@gmail.com>
2025-11-29 16:51:44 +0530

    Updated Program with some changes

--- program.cpp
+++ program.cpp
@@ -4,4 +4,5 @@ using namespace std;
 int main(){
     cout<<"Step 1: git init to initialize folder to the github"<<endl;
     cout<<"git status: for checking the status of the file if it is tracked or untracked"<<endl;
+    cout<<"making changes here and then pushing the file again"<<endl;
 }
