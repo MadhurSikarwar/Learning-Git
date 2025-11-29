826eaa5bd454bcfbe955d077c3ed022a3703787c
MadhurSikarwar <madhursikarwar1234@gmail.com>
2025-11-29 16:47:17 +0530

    Added first cpp program

---
+++ program.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Step 1: git init to initialize folder to the github"<<endl;
+    cout<<"git status: for checking the status of the file if it is tracked or untracked"<<endl;
+}
